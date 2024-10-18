dac2fa8229201a24c66f191253af92751122602a
EArcher <EdgeNaut@gmail.com>
2024-10-18 15:56:53 -0700

    Added Enemies and Health
    The enemies now have functioning AI and the player now has trackable health

---
+++ TutorialProject/Intermediate/Build/Win64/UnrealEditor/Inc/TutorialProject/UHT/TutorialProject.init.gen.cpp
@@ -0,0 +1,29 @@
+// Copyright Epic Games, Inc. All Rights Reserved.
+/*===========================================================================
+	Generated code exported from UnrealHeaderTool.
+	DO NOT modify this manually! Edit the corresponding .h files instead!
+===========================================================================*/
+
+#include "UObject/GeneratedCppIncludes.h"
+PRAGMA_DISABLE_DEPRECATION_WARNINGS
+void EmptyLinkFunctionForGeneratedCodeTutorialProject_init() {}
+	static FPackageRegistrationInfo Z_Registration_Info_UPackage__Script_TutorialProject;
+	FORCENOINLINE UPackage* Z_Construct_UPackage__Script_TutorialProject()
+	{
+		if (!Z_Registration_Info_UPackage__Script_TutorialProject.OuterSingleton)
+		{
+			static const UECodeGen_Private::FPackageParams PackageParams = {
+				"/Script/TutorialProject",
+				nullptr,
+				0,
+				PKG_CompiledIn | 0x00000000,
+				0x8CE30CC8,
+				0x37619232,
+				METADATA_PARAMS(0, nullptr)
+			};
+			UECodeGen_Private::ConstructUPackage(Z_Registration_Info_UPackage__Script_TutorialProject.OuterSingleton, PackageParams);
+		}
+		return Z_Registration_Info_UPackage__Script_TutorialProject.OuterSingleton;
+	}
+	static FRegisterCompiledInInfo Z_CompiledInDeferPackage_UPackage__Script_TutorialProject(Z_Construct_UPackage__Script_TutorialProject, TEXT("/Script/TutorialProject"), Z_Registration_Info_UPackage__Script_TutorialProject, CONSTRUCT_RELOAD_VERSION_INFO(FPackageReloadVersionInfo, 0x8CE30CC8, 0x37619232));
+PRAGMA_ENABLE_DEPRECATION_WARNINGS
