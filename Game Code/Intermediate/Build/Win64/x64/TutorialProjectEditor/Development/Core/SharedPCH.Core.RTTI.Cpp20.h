80e2e81a091c97de964e748dc8f212a8f9f8bf5a
EArcher <EdgeNaut@gmail.com>
2024-11-22 12:10:28 -0800

    Renamed Files for Better Clarification

---
+++ Game Code/Intermediate/Build/Win64/x64/TutorialProjectEditor/Development/Core/SharedPCH.Core.RTTI.Cpp20.h
@@ -0,0 +1,3 @@
+// PCH for Runtime/Core/Public/CoreSharedPCH.h
+#include "C:/Users/EdgeN/OneDrive/Documents/Unreal Projects/Rokos_Basilisk/TutorialProject/Intermediate/Build/Win64/x64/TutorialProjectEditor/Development/Core/SharedDefinitions.Core.RTTI.Cpp20.h"
+#include "Runtime/Core/Public/CoreSharedPCH.h"
---
+++ Game Code/Intermediate/Build/Win64/x64/TutorialProjectEditor/Development/Slate/SharedPCH.Slate.Cpp20.h
@@ -0,0 +1,3 @@
+// PCH for Runtime/Slate/Public/SlateSharedPCH.h
+#include "C:/Users/EdgeN/OneDrive/Documents/Unreal Projects/Rokos_Basilisk/TutorialProject/Intermediate/Build/Win64/x64/TutorialProjectEditor/Development/Slate/SharedDefinitions.Slate.Cpp20.h"
+#include "Runtime/Slate/Public/SlateSharedPCH.h"
