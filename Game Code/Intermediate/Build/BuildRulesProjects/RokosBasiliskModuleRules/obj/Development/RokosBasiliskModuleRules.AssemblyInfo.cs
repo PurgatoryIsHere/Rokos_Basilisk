c6f2e0a51fb0dd2bb93845f8b2d8edb5a3686e50
EArcher <EdgeNaut@gmail.com>
2025-01-26 12:01:48 -0800

    Added Prefabs 16-20 (Arenas)

--- Game Code/Intermediate/Build/BuildRulesProjects/RokosBasiliskModuleRules/obj/Development/RokosBasiliskModuleRules.AssemblyInfo.cs
+++ Game Code/Intermediate/Build/BuildRulesProjects/RokosBasiliskModuleRules/obj/Development/RokosBasiliskModuleRules.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("RokosBasiliskModuleRules")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Development")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+73ef32006c06689bf27fa15050f156527dd6956a")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+d0f9a64f09874412cfc4c9de6b901d0e128b2fb4")]
 [assembly: System.Reflection.AssemblyProductAttribute("RokosBasiliskModuleRules")]
 [assembly: System.Reflection.AssemblyTitleAttribute("RokosBasiliskModuleRules")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
