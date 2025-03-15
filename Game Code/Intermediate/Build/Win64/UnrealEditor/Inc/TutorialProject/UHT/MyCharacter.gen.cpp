939dbbb10c94ad000f8c328c4133f8b3482f5d53
Wasif <163186702+Extreme11ama@users.noreply.github.com>
2025-03-14 21:30:04 -0700

    added basic death screen

--- Game Code/Intermediate/Build/Win64/UnrealEditor/Inc/TutorialProject/UHT/MyCharacter.gen.cpp
+++ Game Code/Intermediate/Build/Win64/UnrealEditor/Inc/TutorialProject/UHT/MyCharacter.gen.cpp
@@ -77,14 +77,14 @@ AMyCharacter::~AMyCharacter() {}
 // End Class AMyCharacter
 
 // Begin Registration
-struct Z_CompiledInDeferFile_FID_GitHub_Rokos_Basilisk_Game_Code_Source_TutorialProject_MyCharacter_h_Statics
+struct Z_CompiledInDeferFile_FID_Users_akhta_Capstone_Rokos_Basilisk_Game_Code_Source_TutorialProject_MyCharacter_h_Statics
 {
 	static constexpr FClassRegisterCompiledInInfo ClassInfo[] = {
 		{ Z_Construct_UClass_AMyCharacter, AMyCharacter::StaticClass, TEXT("AMyCharacter"), &Z_Registration_Info_UClass_AMyCharacter, CONSTRUCT_RELOAD_VERSION_INFO(FClassReloadVersionInfo, sizeof(AMyCharacter), 3143863740U) },
 	};
 };
-static FRegisterCompiledInInfo Z_CompiledInDeferFile_FID_GitHub_Rokos_Basilisk_Game_Code_Source_TutorialProject_MyCharacter_h_2308429023(TEXT("/Script/TutorialProject"),
-	Z_CompiledInDeferFile_FID_GitHub_Rokos_Basilisk_Game_Code_Source_TutorialProject_MyCharacter_h_Statics::ClassInfo, UE_ARRAY_COUNT(Z_CompiledInDeferFile_FID_GitHub_Rokos_Basilisk_Game_Code_Source_TutorialProject_MyCharacter_h_Statics::ClassInfo),
+static FRegisterCompiledInInfo Z_CompiledInDeferFile_FID_Users_akhta_Capstone_Rokos_Basilisk_Game_Code_Source_TutorialProject_MyCharacter_h_2308429023(TEXT("/Script/TutorialProject"),
+	Z_CompiledInDeferFile_FID_Users_akhta_Capstone_Rokos_Basilisk_Game_Code_Source_TutorialProject_MyCharacter_h_Statics::ClassInfo, UE_ARRAY_COUNT(Z_CompiledInDeferFile_FID_Users_akhta_Capstone_Rokos_Basilisk_Game_Code_Source_TutorialProject_MyCharacter_h_Statics::ClassInfo),
 	nullptr, 0,
 	nullptr, 0);
 // End Registration
